d5514f97aba676aca1d488c8dc67d5f830fbb626
bill lau <liubeitongxue@icloud.com>
2025-09-15 13:53:22 +0800

    read blogs.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Flex, Text, Button } from '@radix-ui/themes/dist/cjs/components/index.js'
 import './App.css'
+import blogsData from './assets/blogs.json'
+
+const blogs = blogsData
 
 function App() {
+  console.log(blogs)
   return (
     <>
       <h1 className="text-3xl font-bold underline">
